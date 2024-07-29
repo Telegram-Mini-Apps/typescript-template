91afd0bc9c15f5780a51e8d1bf3cedebad2b0192
Nishcheta Dmitry <nish.dima@gmail.com>
2024-07-30 00:41:57 +0300

    Implement TON Connect page

--- src/initNavigator.ts
+++ src/initNavigator.ts
@@ -1,6 +1,7 @@
 import { initNavigator as initSDKNavigator } from '@telegram-apps/sdk';
 
 export async function initNavigator() {
+  // eslint-disable-next-line @typescript-eslint/ban-types
   const navigator = initSDKNavigator<{}>('app-navigator-state');
 
   // Attach the navigator to the browser history, so it could modify the history and listen to
--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,7 @@ export type MaybeChild = Child | null | undefined | boolean;
 
 export type Children = Child | Child[];
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type PropsWithChildren<P = {}> = P & {
   children?: Children;
 };
