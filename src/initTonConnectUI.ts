9b13ea8138906950ec78b8741b647ba98a516b1b
Nishcheta Dmitry <nish.dima@gmail.com>
2024-08-01 14:38:24 +0300

    Fix tonconnect-manifest URL

--- src/initTonConnectUI.ts
+++ src/initTonConnectUI.ts
@@ -2,7 +2,7 @@ import { TonConnectUI } from '@tonconnect/ui'
 
 export function initTonConnectUI() {
   const tonConnectUI = new TonConnectUI({
-    manifestUrl: new URL('typescript-template/tonconnect-manifest.json', window.location.href).toString(),
+    manifestUrl: new URL('/typescript-template/tonconnect-manifest.json', window.location.href).toString(),
   });
 
   return tonConnectUI;
